ebbd21188c90e2c1202eca4567baaca86158a19b
Rhian McClelland <rhianmcclelland193@hotmail.com>
2024-06-08 10:51:43 +1000

    Add Train Val Split

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from util import load_dataset, get_encode_decode
+from util import load_dataset, get_encode_decode, train_val_split
+import torch
 
 data = load_dataset()
 
@@ -9,3 +10,11 @@ chars = sorted(list(set(data)))
 # Create a mapping from unique characters to indices
 VOCAB_SIZE = len(chars)
 encode, decode = get_encode_decode(chars)
+
+data = torch.tensor(encode(data), dtype=torch.long)
+
+train_data, val_data = train_val_split(data, 0.9)
+
+print(f'Vocab size: {VOCAB_SIZE}')
+print(f'Train data size: {len(train_data)}')
+print(f'Val data size: {len(val_data)}')
