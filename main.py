29629d06bf8cab83b44ce6bc08ed67a75ae99959
Rhian McClelland <rhianmcclelland193@hotmail.com>
2024-06-08 11:04:53 +1000

    Add batching

--- main.py
+++ main.py
@@ -18,3 +18,21 @@ train_data, val_data = train_val_split(data, 0.9)
 print(f'Vocab size: {VOCAB_SIZE}')
 print(f'Train data size: {len(train_data)}')
 print(f'Val data size: {len(val_data)}')
+
+BLOCK_SIZE = 8
+BATCH_SIZE = 4
+SEED = 4
+
+torch.manual_seed(SEED)
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+
+    # Generate Offsets
+    start_idx = torch.randint(0, len(data) - BLOCK_SIZE, (BATCH_SIZE,))
+
+    x = torch.stack([data[idx:idx+BLOCK_SIZE] for idx in start_idx])
+    y = torch.stack([data[idx+1:idx+BLOCK_SIZE+1] for idx in start_idx])
+
+    return x, y
+
